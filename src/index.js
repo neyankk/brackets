f6c469a9491068e2351b23fc9b82ce020fed1f7c
Miroslav <miroslav.shevyrev@bk.ru>
2023-02-23 02:30:55 +0300

    feat:decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let brackets = {
+    ')': '(',
+    '}': '{',
+    ']': '['
+  };
+
+  for (let i = 0; i < str.lenght; i++) {
+    const current = str[i];
+
+    if(isClosedBracket(current)) {
+      if (brackets[current] !== stack.pop()) return false;
+    } else {
+      stack.push(current);
+    }
+  };
+
+  return stack.length === 0;
+
+  function isClosedBracket (ch) {
+    return [')','}',']'].indexOf(ch) > -1;
+  };
+
 }
